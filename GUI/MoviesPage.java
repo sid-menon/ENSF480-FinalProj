1c1362e69233d2c49c26302d04ea550d3a71c9d2
sid-menon <86627011+sid-menon@users.noreply.github.com>
2022-12-01 20:20:52 -0700

    Add files via upload

---
+++ GUI/MoviesPage.java
@@ -0,0 +1,13 @@
+import javax.swing.*;  
+  
+class MoviesPage extends JFrame //list of movies from user to choose from 
+{  
+    MoviesPage()  
+    {  
+        setDefaultCloseOperation(javax.swing.  
+        WindowConstants.DISPOSE_ON_CLOSE);  
+        setTitle("Movie List");  
+        setSize(400, 200);
+        //display list of movies  
+    }  
+}
