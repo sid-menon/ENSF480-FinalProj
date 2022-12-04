ac2b89a87ad45032f3416dcdecc09204399a0918
nevrendng <101433464+nevrendng@users.noreply.github.com>
2022-12-03 18:01:01 -0700

    Update Seat.java
    updated with early registration and if seats are occupied

--- Controller/Theatre_app/src/controller/Seat.java
+++ Controller/Theatre_app/src/controller/Seat.java
@@ -1,16 +1,67 @@
 package controller;
 
-public class Seat {
-    private int roomNumber;
-    private char row;
-    private int col;
+public class Seat 
+{
+	private int row;
+	private int col;
+	private boolean isEarlyRegistration;
+	private boolean isTaken;
+	
 
-    public Seat(int roomNumber, char row, int col) {
-        this.roomNumber = roomNumber;
-        this.row = row;
-        this.col = col;
-    }
+	public Seat(int row, int col) 
+    {
+		this.row = row;
+		this.col = col;
+		isEarlyRegistration = false;
+		isTaken = false;
+	}
+	
+	
+	boolean occupySeat(boolean isEarly) 
+    {
+		if (isTaken == false) {
+			if (isEarly == true) 
+            {
+				isEarlyRegistration = true;
+			}
+			isTaken = true;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	boolean unoccupySeat() 
+    {
+		if (isTaken == true) 
+        {
+			isEarlyRegistration = false;
+			isTaken = false;
+			return true;
+		} else 
+        {
+			return false;
+		}
+	}
+	
+	public int getrow() 
+    {
+		return row;
+	}
 
+	public int getcol() 
+    {
+		return col;
+	}
 
+	public boolean isEarlyRegistration() 
+    {
+		return isEarlyRegistration;
+	}
+
+	public boolean isTaken() 
+    {
+		return isTaken;
+	}
 
 }
