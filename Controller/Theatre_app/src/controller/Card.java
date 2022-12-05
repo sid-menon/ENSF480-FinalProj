0bcfc9449328cc956edf0f12202281c60a551795
Charles.H <100550614+fbcharles747@users.noreply.github.com>
2022-12-05 12:32:43 -0700

    Add back the Card class

---
+++ Controller/Theatre_app/src/controller/Card.java
@@ -0,0 +1,54 @@
+
+package controller;
+
+
+public class Card {
+
+    private String cardNumber;
+
+    private String exDate;
+
+    private int csv;
+
+
+    public Card(String cardNumber, String exDate, int csv) {
+        this.cardNumber = cardNumber;
+        this.exDate = exDate;
+        this.csv = csv;
+    }
+
+
+    public void updateCardInfo(String cardNumber, String exDate, int csv) {
+        this.cardNumber = cardNumber;
+        this.exDate = exDate;
+        this.csv = csv;
+    }
+
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+
+    public String getExDate() {
+        return exDate;
+    }
+
+    public void setExDate(String exDate) {
+        this.exDate = exDate;
+    }
+
+    public int getCsv() {
+        return csv;
+    }
+
+
+    public void setCsv(int csv) {
+        this.csv = csv;
+    }
+}
